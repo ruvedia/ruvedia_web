bdeb5976b33770f8e4bc6d6357fed5818fb1c527
Fran Cervantes Ruiz <francervantesruiz@Frans-MacBook-Air.local>
2026-06-24 19:50:18 +0200

    fix: change site URL to https://ruvedia.com to resolve Astro native CSRF blocker

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://www.ruvedia.com',
+  site: 'https://ruvedia.com',
   adapter: cloudflare(),
 
   vite: {
